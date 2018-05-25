c62e9bcf1a3a49fd7068b61ce759313e20d03ba9
Veltosian <wizardnep@gmail.com>
2018-05-24 22:13:23 -0700

    Added gameboard replace() to put a marker over the bet Fixed small issue in gameboardNaked asset: removed '0' and '00'

--- python/display_r.py
+++ python/display_r.py
@@ -4,21 +4,75 @@ class RouletteBoard():
     def __init__(self):
         self.board = []
         self.token = []
+        self.board = self.getAsset("Asset:gameboard")
+        self.setupElements()
+
+    def setupElements(self):
+        '''
+        Create dictionary of each element with indexes
+        Generate indexes of the 3 rows of numbers with math
+        Index includes: [start row, start column, width]
+        '''
+        self.betElements = {}
+        
+        row = [(x+1)*3 for x in range(12)]
+        for i in range(len(row)):
+            self.betElements[row[i]] = [1,4+3*i,2]
+
+        row = [(x+1)*3-1 for x in range(12)]
+        for i in range(len(row)):
+            self.betElements[row[i]] = [3,4+3*i,2]
+
+        row = [(x+1)*3-2 for x in range(12)]
+        for i in range(len(row)):
+            self.betElements[row[i]] = [5,4+3*i,2]
+
+        self.betElements[-1] = [2,1,2]
+        self.betElements[-0] = [6,1,2]
+        self.betElements["2to1[0]"] = [1,41,4]
+        self.betElements["2to1[1]"] = [3,41,4]
+        self.betElements["2to1[2]"] = [5,41,4]
+        self.betElements["1st.12"]  = [7,6,6]
+        self.betElements["2nd.12"]  = [7,18,6]
+        self.betElements["3rd.12"]  = [7,31,6]
+        self.betElements["1to18"]   = [9,4,5]
+        self.betElements["EVEN"]    = [9,10,4]
+        self.betElements["RED"]     = [9,16,3]
+        self.betElements["BLACK"]   = [9,22,5]
+        self.betElements["ODD"]     = [9,29,3]
+        self.betElements["19to36"]  = [9,34,6]
+        
+    def replace(self,element):
+        #TODO Replace with something instead
+        #     of a placeholder
+        board = self.board
+        idx = self.betElements[element]
+        row = idx[0]
+        col = idx[1]
+        width = idx[2]
+        for i in range(width):
+            board[row][col+i] = "Z"
+        return board
+        
+    def getAsset(self, asset):
+        board = []
         f=open("./assets/gameboard.txt", "r")
         assetLine = f.readline()
-        while assetLine != "Asset:gameboard\n":
+        while assetLine != "".join([asset,"\n"]):
             assetLine = f.readline()
         assetLine = f.readline()
         while assetLine != "\n":
-            self.board.append(assetLine)
+            board.append(list(assetLine))
+            del board[-1][-1]
             assetLine = f.readline()
             f.close
-        self.board[-1] = self.board[-1].strip()
+        return board
         
     def display(self):
         dispBoard = self.board
+        for i in range(len(dispBoard)):
+            print("".join(dispBoard[i]))
         
-        print("".join(dispBoard))
 
     def getToken(self):
         pass
@@ -27,3 +81,27 @@ class RouletteBoard():
 rBoard = RouletteBoard()
 
 rBoard.display()
+
+testBoard = rBoard.replace("19to36")
+for i in range(len(testBoard)):
+    print("".join(testBoard[i]))
+
+print(rBoard.betElements[9])
+
+
+#Where I'm at: I added the elements dictionary so that we have the
+# index and width of each element on the board. We can replace
+# an element with the .replace(element) function. The replace()
+# function replaces with a "Z" for now.
+
+#Need to add the ability to replace with a sprite or something.
+#Maybe a sprite isn't needed
+# though and we could just use some better placeholder.
+
+#Also need to add a way to display the gameboard with the replaced
+# element. I don't want to update the self.board  variable because
+# then we'd lose what was originally in that place. Just want to
+# display a temporary board with the marker on some element.
+
+#Eventually it will be sweet to add the ability to move around the
+# gameboard using the arrow keys with a blinking marker. 
